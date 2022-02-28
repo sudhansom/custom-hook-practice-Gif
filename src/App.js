0f0048cc78f07915311e6441a0db040c378b17cd
bhuwan <bkspoudel@gmail.com>
2022-02-28 18:00:45 +0100

    snake case managed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from "react";
 
 import "./App.css";
-import Random_V1 from "./components/Random_V1";
-import Tag_V1 from "./components/Tag_V1";
-import Random_V2 from "./components/Random_v2";
-import Tag_V2 from "./components/Tag_v2";
+// import Random_V1 from "./components/Random_V1";
+// import Tag_V1 from "./components/Tag_V1";
+import Random from "./components/Random_v2";
+import Tag from "./components/Tag_v2";
 
 const App = () => (
   <div>
     <h1>A Simple Gif App</h1>
     <div className="main-container">
-      <Random_V2 />
-      <Tag_V2 />
+      <Random />
+      <Tag />
     </div>
   </div>
 );
