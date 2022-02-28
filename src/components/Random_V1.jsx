07c8471548eb90092f0dce1c6251b21f98ecd7aa
bhuwan <bkspoudel@gmail.com>
2022-02-28 15:24:07 +0100

    added a button for a new one

--- src/components/Random_V1.jsx
+++ src/components/Random_V1.jsx
@@ -5,21 +5,28 @@ const API_KEY = process.env.REACT_APP_API_KEY;
 
 function Random_V1() {
   const [gif, setGif] = useState("");
+
+  const fetchGif = async () => {
+    const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
+    const { data } = await axios.get(url);
+    console.log(data.data.images.downsized_large.url);
+    const imageSrc = data.data.images.downsized_large.url;
+    setGif(imageSrc);
+  };
+
   useEffect(() => {
-    const fetchGif = async () => {
-      const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
-      const { data } = await axios.get(url);
-      console.log(data.data.images.downsized_large.url);
-      const imageSrc = data.data.images.downsized_large.url;
-      setGif(imageSrc);
-    };
     fetchGif();
   }, []);
+
+  const handleClick = () => {
+    fetchGif();
+  };
 
   return (
     <div>
       <h1>Random</h1>
       <img width="500" src={gif} alt="random" />
+      <button onClick={handleClick}>ANOTHER ONE</button>
     </div>
   );
 }
