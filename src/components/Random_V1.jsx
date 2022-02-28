9f63078752a6b998f547072cd931391e3028dc8b
bhuwan <bkspoudel@gmail.com>
2022-02-28 15:06:31 +0100

    used async useEffect in Random_v1

--- src/components/Random_V1.jsx
+++ src/components/Random_V1.jsx
@@ -1,8 +1,20 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 function Random_V1() {
+  const [gif, setGif] = useState("");
+  useEffect(() => {
+    const fetchGif = async () => {
+      const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
+      const data = await axios.get(url);
+      const { imageSrc } = data.data.images.downsized_large.url;
+      setGif(imageSrc);
+    };
+    fetchGif();
+  });
+
   return (
     <>
       <h1>Random</h1>
